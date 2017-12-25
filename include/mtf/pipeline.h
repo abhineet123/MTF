b42d2ded6eacbc7736806101c6eea0c8c8c560bd
abhineet123 <asingh1@ualberta.ca>
2017-12-24 20:19:51 -0700

    minor fix

--- include/mtf/pipeline.h
+++ include/mtf/pipeline.h
@@ -49,17 +49,17 @@ inline utils::InputBase* getInput(char _pipeline_type){
 	case VISP_PIPELINE:
 	{
 		utils::InputVPParams _params(&_base_params,
-			visp_usb_n_buffers,
-			static_cast<utils::InputVPParams::VpResUSB>(visp_usb_res),
-			static_cast<utils::InputVPParams::VpFpsUSB>(visp_usb_fps),
-			static_cast<utils::InputVPParams::VpResFW>(visp_fw_res),
-			static_cast<utils::InputVPParams::VpFpsFW>(visp_fw_fps),
-			static_cast<utils::InputVPParams::VpDepthPGFW>(visp_pg_fw_depth),
-			visp_pg_fw_print_info,
-			visp_pg_fw_shutter_ms,
-			visp_pg_fw_gain,
-			visp_pg_fw_exposure,
-			visp_pg_fw_brightness
+			vp_usb_n_buffers,
+			static_cast<utils::InputVPParams::VpResUSB>(vp_usb_res),
+			static_cast<utils::InputVPParams::VpFpsUSB>(vp_usb_fps),
+			static_cast<utils::InputVPParams::VpResFW>(vp_fw_res),
+			static_cast<utils::InputVPParams::VpFpsFW>(vp_fw_fps),
+			static_cast<utils::InputVPParams::VpDepthPGFW>(vp_pg_fw_depth),
+			vp_pg_fw_print_info,
+			vp_pg_fw_shutter_ms,
+			vp_pg_fw_gain,
+			vp_pg_fw_exposure,
+			vp_pg_fw_brightness
 			);
 		return new utils::InputVP(&_params);
 	}
