fcad2c8c9c2fe7a67796b6c7ef7a11ab0557fadd
abhineet123 <asingh1@ualberta.ca>
2018-01-05 22:29:09 -0700

    updates in CV3;

--- ThirdParty/CV3/include/mtf/ThirdParty/CV3/CV3.h
+++ ThirdParty/CV3/include/mtf/ThirdParty/CV3/CV3.h
@@ -1,26 +1,20 @@
-#ifndef MTF_VISP_H
-#define MTF_VISP_H
+//#if CV_MAJOR_VERSION >= 3
+
+#ifndef MTF_CV3_H
+#define MTF_CV3_H
 
 #include "mtf/TrackerBase.h"
-#include <visp3/tt/vpTemplateTrackerWarp.h>
-#include <visp3/tt/vpTemplateTracker.h>
-#include <memory>
+#include <opencv2/tracking.hpp>
 
-struct ViSPParams{
-	enum class SSMType{
-		Homography, SL3, Affine,
-		Similarity, Isometry, Translation
+struct CV3Params{
+	enum class TrackerType{
+		MIL,
+		BOOSTING,
+		MEDIANFLOW,
+		TLD,
+		KCF
 	};
-	enum class SMType{
-		FCLK, ICLK, FALK, ESM
-	};
-	enum class AMType{
-		SSD, ZNCC, MI
-	};
-
-	SMType sm_type;
-	AMType am_type;
-	SSMType ssm_type;
+	TrackerType tracker_type;
 
 	int max_iters;
 	int resx, resy;
@@ -29,8 +23,7 @@ struct ViSPParams{
 	int pyr_n_levels;
 	int pyr_level_to_stop;
 
-	ViSPParams(SMType _sm_type, AMType _am_type,
-		SSMType _ssm_type,
+	CV3Params(TrackerType _sm_type,
 		int _max_iters,
 		int _resx,
 		int _resy,
@@ -39,36 +32,33 @@ struct ViSPParams{
 		int _pyr_n_levels,
 		int _pyr_level_to_stop);
 
-	ViSPParams(const ViSPParams *params = nullptr);
+	CV3Params(const CV3Params *params = nullptr);
+	std::string toString(TrackerType _detector_type);
+
 };
 
-class ViSP : public mtf::TrackerBase{
+class CV3 : public mtf::TrackerBase{
 public:
 
-	typedef ViSPParams ParamType;
-	typedef ParamType::SMType SMType;
-	typedef ParamType::AMType AMType;
-	typedef ParamType::SSMType SSMType;
-	typedef std::unique_ptr<vpTemplateTracker> VPTrackerPtr;
-	typedef std::unique_ptr<vpTemplateTrackerWarp> VPWarpPtr;
+	typedef CV3Params ParamType;
+	typedef ParamType::TrackerType TrackerType;
 
-	ViSP(const ParamType *visp_params);	
+	CV3(const ParamType *cv3_params);	
 	void initialize(const cv::Mat &corners) override;
 	void update() override;
 	const cv::Mat& getRegion() override{ return cv_corners_mat; }
 	void setRegion(const cv::Mat& corners) override;
 	void setImage(const cv::Mat &img) override;
 	int inputType() const override{ return CV_8UC1; }
-	void setOptimalSamplingRatio(const cv::Mat &corners);
 	void updateCorners();
 
 protected:
 	ParamType params;
+	cv::Ptr<cv::Tracker> tracker;
 	cv::Mat curr_img_cv;
-	VPWarpPtr warp;
-	VPTrackerPtr tracker;
 	
 private:
-	virtual ~ViSP(){}
+	virtual ~CV3(){}
 };
 #endif
+//#endif
--- ThirdParty/CV3/src/CV3.cc
+++ ThirdParty/CV3/src/CV3.cc
@@ -1,42 +1,20 @@
-#include "mtf/ThirdParty/ViSP/ViSP.h"
-#include <visp3/core/vpImage.h>
-#include <visp3/tt/vpTemplateTrackerSSD.h>
-#include <visp3/tt/vpTemplateTrackerSSDForwardAdditional.h>
-#include <visp3/tt/vpTemplateTrackerSSDForwardCompositional.h>
-#include <visp3/tt/vpTemplateTrackerSSDInverseCompositional.h>
-#include <visp3/tt/vpTemplateTrackerSSDESM.h>
-#include <visp3/tt/vpTemplateTrackerZNCCForwardAdditional.h>
-#include <visp3/tt/vpTemplateTrackerZNCCInverseCompositional.h>
-
-#include <visp3/tt/vpTemplateTrackerWarpAffine.h>
-#include <visp3/tt/vpTemplateTrackerWarpHomography.h>
-#include <visp3/tt/vpTemplateTrackerWarpHomographySL3.h>
-#include <visp3/tt/vpTemplateTrackerWarpSRT.h>
-#include <visp3/tt/vpTemplateTrackerWarpTranslation.h>
-#include <visp3/tt/vpTemplateTrackerWarpRT.h>
-
-#ifdef VISP_HAVE_MODULE_TT_MI
-#  include <visp3/tt_mi/vpTemplateTrackerMIESM.h>
-#  include <visp3/tt_mi/vpTemplateTrackerMIForwardAdditional.h>
-#  include <visp3/tt_mi/vpTemplateTrackerMIForwardCompositional.h>
-#  include <visp3/tt_mi/vpTemplateTrackerMIInverseCompositional.h>
-#endif
+#include "mtf/ThirdParty/CV3/CV3.h"
 
 #include "mtf/Utilities/miscUtils.h"
 #include "mtf/Utilities/excpUtils.h"
 
-#define VISP_SM 0
-#define VISP_AM 0
-#define VISP_SSM 0
-#define  VISP_MAX_ITERS 30
-#define  VISP_SAMPLING_RES 50
-#define  VISP_LAMBDA 0.001
-#define  VISP_THRESH_GRAD 60
-#define  VISP_PYR_N_LEVELS 0
-#define  VISP_PYR_LEVEL_TO_STOP 1
+#define CV3_SM 0
+#define CV3_AM 0
+#define CV3_SSM 0
+#define CV3_MAX_ITERS 30
+#define CV3_SAMPLING_RES 50
+#define CV3_LAMBDA 0.001
+#define CV3_THRESH_GRAD 60
+#define CV3_PYR_N_LEVELS 0
+#define CV3_PYR_LEVEL_TO_STOP 1
 
 
-ViSPParams::ViSPParams(SMType _sm_type, AMType _am_type,
+CV3Params::CV3Params(SMType _sm_type, AMType _am_type,
 	SSMType _ssm_type,
 	int _max_iters,
 	int _resx,
@@ -56,7 +34,7 @@ ViSPParams::ViSPParams(SMType _sm_type, AMType _am_type,
 	pyr_n_levels(_pyr_n_levels),
 	pyr_level_to_stop(_pyr_level_to_stop){}
 
-ViSPParams::ViSPParams(const ViSPParams *params) :
+CV3Params::CV3Params(const CV3Params *params) :
 sm_type(static_cast<SMType>(VISP_SM)),
 am_type(static_cast<AMType>(VISP_AM)),
 ssm_type(static_cast<SSMType>(VISP_SSM)),
@@ -82,10 +60,31 @@ pyr_level_to_stop(VISP_PYR_LEVEL_TO_STOP){
 	}
 }
 
-ViSP::ViSP(const ParamType *visp_params) : TrackerBase(),
-params(visp_params),
+std::string CV3Params::toString(TrackerType _tracker_type){
+	switch(_tracker_type){
+	case TrackerType::MIL:
+		return "MIL";
+	case TrackerType::BOOSTING:
+		return "BOOSTING";
+	case TrackerType::MEDIANFLOW:
+		return "MEDIANFLOW";
+	case TrackerType::TLD:
+		return "TLD";
+	case TrackerType::KCF:
+		return "KCF";
+	default:
+		throw utils::InvalidArgument(cv::format(
+			"Invalid tracker type provided %d", static_cast<int>(_tracker_type)));
+	}
+}
+
+CV3::CV3(const ParamType *cv3_params) : TrackerBase(),
+params(cv3_params),
 warp(nullptr), tracker(nullptr){
-	printf("Using ViSP Template Tracker with:\n");
+	std::string tracker_type_str = params.toString(params.tracker_type);
+
+	printf("Using CV3 Tracker with:\n");
+	printf("tracker_type: %s\n", tracker_type_str.c_str());
 	printf("max_iters: %d\n", params.max_iters);
 	printf("resx: %d\n", params.resx);
 	printf("resy: %d\n", params.resy);
@@ -94,193 +93,18 @@ warp(nullptr), tracker(nullptr){
 	printf("pyr_n_levels: %d\n", params.pyr_n_levels);
 	printf("pyr_level_to_stop: %d\n", params.pyr_level_to_stop);
 
-	switch(params.sm_type) {
-	case SMType::FCLK:
-		printf("Using FCLK SM\n");
-		break;
-	case SMType::ICLK:
-		printf("Using ICLK SM\n");
-		break;
-	case SMType::FALK:
-		printf("Using FALK SM\n");
-		break;
-	case SMType::ESM:
-		printf("Using ESM SM\n");
-		break;
-	default:
-		throw mtf::utils::InvalidArgument("Invalid search method provided");
-	}
-
-	switch(params.ssm_type) {
-	case SSMType::Homography:
-		printf("Using Homography SSM\n");
-		warp.reset(new vpTemplateTrackerWarpHomography);
-		break;
-	case SSMType::SL3:
-		printf("Using SL3 SSM\n");
-		warp.reset(new vpTemplateTrackerWarpHomographySL3);
-		break;
-	case SSMType::Affine:
-		printf("Using Affine SSM\n");
-		warp.reset(new vpTemplateTrackerWarpAffine);
-		break;
-	case SSMType::Similarity:
-		printf("Using Similarity SSM\n");
-		warp.reset(new vpTemplateTrackerWarpSRT);
-		break;
-	case SSMType::Translation:
-		printf("Using Translation SSM\n");
-		warp.reset(new vpTemplateTrackerWarpTranslation);
-		break;
-#ifdef VISP_HAVE_MODULE_TT_MI
-	case SSMType::Isometry:
-		printf("Using Isometry SSM\n");
-		warp.reset(new vpTemplateTrackerWarpRT);
-		break;
-#endif
-	default:
-		throw mtf::utils::InvalidArgument("Invalid state space model provided");
-	}
-
-	switch(params.am_type) {
-	case AMType::SSD:
-		switch(params.sm_type) {
-		case SMType::FCLK:
-			tracker.reset(new vpTemplateTrackerSSDForwardCompositional(warp.get()));
-			break;
-		case SMType::ICLK:
-			tracker.reset(new vpTemplateTrackerSSDInverseCompositional(warp.get()));
-			break;
-		case SMType::FALK:
-			tracker.reset(new vpTemplateTrackerSSDForwardAdditional(warp.get()));
-			break;
-		case SMType::ESM:
-			tracker.reset(new vpTemplateTrackerSSDESM(warp.get()));
-			break;
-		default:
-			throw mtf::utils::InvalidArgument("Invalid search method provided for SSD");
-		}
-		printf("Using SSD AM\n");
-		break;
-	case AMType::ZNCC:
-		switch(params.sm_type) {
-		case SMType::FCLK:
-			throw mtf::utils::InvalidArgument("Invalid search method provided for ZNCC");
-		case SMType::ICLK:
-			tracker.reset(new vpTemplateTrackerZNCCInverseCompositional(warp.get()));
-			break;
-		case SMType::FALK:
-			tracker.reset(new vpTemplateTrackerZNCCForwardAdditional(warp.get()));
-			break;
-		case SMType::ESM:
-			throw mtf::utils::InvalidArgument("Invalid search method provided for ZNCC");
-		default:
-			throw mtf::utils::InvalidArgument("Invalid search method provided for ZNCC");
-		}
-		printf("Using ZNCC AM\n");
-		break;
-	case AMType::MI:
-		switch(params.sm_type) {
-		case SMType::FCLK:
-			tracker.reset(new vpTemplateTrackerMIForwardCompositional(warp.get()));
-			break;
-		case SMType::ICLK:
-			tracker.reset(new vpTemplateTrackerMIInverseCompositional(warp.get()));
-			break;
-		case SMType::FALK:
-			tracker.reset(new vpTemplateTrackerMIForwardAdditional(warp.get()));
-			break;
-		case SMType::ESM:
-			tracker.reset(new vpTemplateTrackerMIESM(warp.get()));
-			break;
-		default:
-			throw mtf::utils::InvalidArgument("Invalid search method provided for MI");
-		}
-		printf("Using MI AM\n");
-		break;
-	default:
-		throw mtf::utils::InvalidArgument("Invalid appearance model provided");
-	}
-
-
-	tracker->setLambda(params.lambda);
-	if(params.thresh_grad > 0) {
-		tracker->setThresholdGradient(params.thresh_grad);
-	}
-	tracker->setIterationMax(params.max_iters);
-	if(params.pyr_n_levels > 0) {
-		tracker->setPyramidal(params.pyr_n_levels, params.pyr_level_to_stop);
-	}
-
+	tracker = cv::Tracker::create(tracker_type_str);
 	cv_corners_mat.create(2, 4, CV_64FC1);
 
 }
-void ViSP::setImage(const cv::Mat &img){
+void CV3::setImage(const cv::Mat &img){
 	curr_img_cv = img;
 }
-void ViSP::setOptimalSamplingRatio(const cv::Mat &corners){
 
-	cv::Rect_<double> best_fit_rect = mtf::utils::getBestFitRectangle<double>(corners);
-	int size_x = static_cast<int>(best_fit_rect.width);
-	int size_y = static_cast<int>(best_fit_rect.height);
-
-	int n_pix_desired = params.resx*params.resy;
-
-	int sampling_ratio_x = static_cast<int>(ceil(static_cast<double>(size_x) /
-		static_cast<double>(params.resx)));
-	int sampling_ratio_y = static_cast<int>(ceil(static_cast<double>(size_y) /
-		static_cast<double>(params.resy)));
-
-	int sampling_res_x = size_x / sampling_ratio_x;
-	int sampling_res_y = size_y / sampling_ratio_y;
-	int n_pix = sampling_res_x*sampling_res_y;
-	int n_pix_diff = abs(n_pix_desired - n_pix);
-
-	int n_pix_diff_inc_y = n_pix_diff + 1, n_pix_diff_inc_x = n_pix_diff + 1;
-	printf("Candidates: \n");
-	printf("Ratio: %d x %d :: Res: %d x %d n_pix: %d \n", sampling_ratio_x, sampling_ratio_y,
-		sampling_res_x, sampling_res_y, n_pix);
-
-	if(sampling_ratio_x > 1){
-		int sampling_res_x_inc = size_x / (sampling_ratio_x - 1);
-		int n_pix_inc_x = sampling_res_x_inc*sampling_res_y;
-		n_pix_diff_inc_x = abs(n_pix_desired - n_pix_inc_x);
-		printf("Ratio: %d x %d :: Res: %d x %d n_pix: %d \n", sampling_ratio_x - 1, sampling_ratio_y,
-			sampling_res_x_inc, sampling_res_y, n_pix_inc_x);
-	}
-	if(sampling_ratio_y > 1){
-		int sampling_res_y_inc = size_y / (sampling_ratio_y - 1);
-		int n_pix_inc_y = sampling_res_x*sampling_res_y_inc;
-		n_pix_diff_inc_y = abs(n_pix_desired - n_pix_inc_y);
-		printf("Ratio: %d x %d :: Res: %d x %d n_pix: %d \n", sampling_ratio_x, sampling_ratio_y - 1,
-			sampling_res_x, sampling_res_y_inc, n_pix_inc_y);
-	}
-
-	if(n_pix_diff < n_pix_diff_inc_x){
-		if(n_pix_diff > n_pix_diff_inc_y){
-			--sampling_ratio_y;
-		}
-	} else{
-		if(n_pix_diff_inc_x > n_pix_diff_inc_y){
-			--sampling_ratio_y;
-		} else{
-			--sampling_ratio_x;
-		}
-	}
-	sampling_res_x = size_x / sampling_ratio_x;
-	sampling_res_y = size_y / sampling_ratio_y;
-	n_pix = sampling_res_x*sampling_res_y;
-
-	printf("Chosen: \n");
-	printf("Ratio: %d x %d :: Res: %d x %d n_pix: %d \n", sampling_ratio_x, sampling_ratio_y,
-		sampling_res_x, sampling_res_y, n_pix);
-
-	tracker->setSampling(sampling_ratio_y, sampling_ratio_x);
-}
 // overloaded variants that avoid passing the image repeatedly by sharing
 // memory with the buffer where the images are read in by the input pipeline
-void ViSP::initialize(const cv::Mat &corners){
-	cout << "ViSP::initialize::corners:\n" << corners << "\n";
+void CV3::initialize(const cv::Mat &corners){
+	cout << "CV3::initialize::corners:\n" << corners << "\n";
 
 	setOptimalSamplingRatio(corners);
 	std::vector<vpImagePoint> init_corners;
@@ -307,7 +131,7 @@ void ViSP::initialize(const cv::Mat &corners){
 	}
 }
 
-void ViSP::update(){
+void CV3::update(){
 	try{
 		vpImage<unsigned char> curr_img_vp((unsigned char* const)curr_img_cv.data, curr_img_cv.rows, curr_img_cv.cols, true);
 		tracker->track(curr_img_vp);
@@ -318,7 +142,7 @@ void ViSP::update(){
 }
 
 // return true if the tracker requires the raw RGB image
-void ViSP::updateCorners(){
+void CV3::updateCorners(){
 	// Get the estimated parameters
 	vpColVector p = tracker->getp();
 	// Instantiate and get the reference zone
@@ -361,7 +185,7 @@ void ViSP::updateCorners(){
 	cv_corners_mat.at<double>(1, 3) = corners[1].get_i();
 }
 
-void ViSP::setRegion(const cv::Mat& corners){
+void CV3::setRegion(const cv::Mat& corners){
 	try{
 		tracker->resetTracker();
 		initialize(corners);
