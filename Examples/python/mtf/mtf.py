abcf3696bd62d2d72dd0128ee368f6e54c75fe58
abhineet123 <asingh1@ualberta.ca>
2018-08-04 20:13:56 -0600

    8:11 PM added open function to mtf.VideoCapture; 8:12 PM added width and height as alternative info_ids for VideoCapture.get;

--- Examples/python/mtf/mtf.py
+++ Examples/python/mtf/mtf.py
@@ -3,28 +3,29 @@ import os
 
 class VideoCapture():
     def __init__(self, cap_src='', cap_args=''):
+        self.open(cap_src, cap_args)
 
+    def open(self, cap_src='', cap_args=''):
         if cap_src:
             try:
-                self.cam_id = int(cap_src)
-                self.mtf_args = 'img_source u'
+                mtf_args = 'img_source u'
             except ValueError:
                 if os.path.isdir(cap_src):
-                    self.mtf_args = 'img_source j seq_path {:s} seq_fmt jpg'.format(cap_src)
+                    mtf_args = 'img_source j seq_path {:s} seq_fmt jpg'.format(cap_src)
                 elif os.path.isfile(cap_src):
                     seq_fmt = os.path.splitext(os.path.basename(cap_src))[1][1:]
-                    self.mtf_args = 'img_source m seq_path {:s} seq_fmt {}'.format(cap_src, seq_fmt)
+                    mtf_args = 'img_source m seq_path {:s} seq_fmt {}'.format(cap_src, seq_fmt)
                 else:
                     raise IOError('Invalid capture source: {}'.format(cap_src))
         else:
-            self.mtf_args = ''
+            mtf_args = ''
 
         if cap_args:
-            if self.mtf_args:
-                self.mtf_args = '{} {}'.format(self.mtf_args, cap_args)
+            if mtf_args:
+                mtf_args = '{} {}'.format(mtf_args, cap_args)
             else:
-                self.mtf_args = cap_args
-        if not pyMTF2.init(self.mtf_args):
+                mtf_args = cap_args
+        if not pyMTF2.init(mtf_args):
             raise SystemError('MTF input pipeline creation was unsuccessful')
         else:
             print('MTF input pipeline created successfully')
@@ -34,9 +35,9 @@ class VideoCapture():
         if src_img is None:
             print('Frame extraction was unsuccessful')
             return None
-        if info_id == 3:
+        if info_id == 3 or info_id == 'width':
             return src_img.shape[1]
-        elif info_id == 4:
+        elif info_id == 4 or info_id == 'height':
             return src_img.shape[0]
         else:
             print('iInvalid info ID: {}'.format(info_id))
